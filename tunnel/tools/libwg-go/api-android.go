1a91da9780f95a1942938a0d42d7e294fb946046
mustaddon <mustaddon@gmail.com>
2026-05-05 16:35:04 +0200

    tst

--- tunnel/tools/libwg-go/api-android.go
+++ tunnel/tools/libwg-go/api-android.go
@@ -20,11 +20,11 @@ import (
 	"strings"
 	"unsafe"
 
+	"github.com/mustaddon/wireguard-go/conn"
+	"github.com/mustaddon/wireguard-go/device"
+	"github.com/mustaddon/wireguard-go/ipc"
+	"github.com/mustaddon/wireguard-go/tun"
 	"golang.org/x/sys/unix"
-	"golang.zx2c4.com/wireguard/conn"
-	"golang.zx2c4.com/wireguard/device"
-	"golang.zx2c4.com/wireguard/ipc"
-	"golang.zx2c4.com/wireguard/tun"
 )
 
 type AndroidLogger struct {
@@ -213,7 +213,7 @@ func wgVersion() *C.char {
 		return C.CString("unknown")
 	}
 	for _, dep := range info.Deps {
-		if dep.Path == "golang.zx2c4.com/wireguard" {
+		if dep.Path == "github.com/mustaddon/wireguard-go" {
 			parts := strings.Split(dep.Version, "-")
 			if len(parts) == 3 && len(parts[2]) == 12 {
 				return C.CString(parts[2][:7])
